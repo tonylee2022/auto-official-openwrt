50b1c77cb8265c7c45abd012038db55689078096
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2023-11-26 06:29:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# Add luci-app-amlogic
+# svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/libcryptopp package/feeds/smpackage/libcryptopp
+rm -rf feeds/smpackage/{luci-app-bandwidthd,amule}
