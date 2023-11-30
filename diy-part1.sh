9597d844202cae254a9754b139df2bc9c157dba2
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2023-11-30 13:22:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git openwrt-passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+
